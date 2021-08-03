e8745f3f5fd8aeac12bf63cbc27af436b5094fb3
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2021-08-03 13:06:22 -0400

    add: api config

--- app/src/main/java/com/example/test_shopping_app/data/SaleApi.kt
+++ app/src/main/java/com/example/test_shopping_app/data/SaleApi.kt
@@ -1,8 +1,22 @@
 package com.example.test_shopping_app.data
 
-class SaleApi(
-    private val service : SaleService
-) {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SaleApi(){
+
+    companion object {
+        private var INSTANCE: Retrofit? = null
 
 
+        fun getInstance(): Retrofit? {
+            if (INSTANCE == null) {
+                INSTANCE = Retrofit.Builder()
+                    .baseUrl("http://www.mocky.io/v2/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return INSTANCE
+        }
+    }
 }
